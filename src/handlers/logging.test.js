3ee6152f198c732ce258948a33775138140538ef
David Joy <davidjoy@users.noreply.github.com>
2019-09-18 16:14:20 -0400

    Update src/handlers/logging.test.js
    Co-Authored-By: Adam Butterworth <abutterworth@edx.org>

--- src/handlers/logging.test.js
+++ src/handlers/logging.test.js
@@ -8,7 +8,7 @@ jest.mock('@edx/frontend-logging', () => ({
   configureLoggingService: jest.fn(),
 }));
 
-it('should configure logging with the logging servie to use', () => {
+it('should configure logging with the logging service to use', () => {
   const app = {
     loggingService: 'logging service',
   };
